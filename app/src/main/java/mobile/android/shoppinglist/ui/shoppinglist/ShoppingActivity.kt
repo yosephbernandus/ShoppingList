c57f16bb02793f3c1c87a2a6a535a142834d330e
Yosephbernandus <yosephbernandus@gmail.com>
2022-03-19 23:17:02 +0700

    repository, viewmodel, viewmodelfactory

--- app/src/main/java/mobile/android/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/mobile/android/shoppinglist/ui/shoppinglist/ShoppingActivity.kt
@@ -2,11 +2,21 @@ package mobile.android.shoppinglist.ui.shoppinglist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import mobile.android.shoppinglist.R
+import mobile.android.shoppinglist.data.db.ShoppingDatabase
+import mobile.android.shoppinglist.data.repositories.ShoppingRepository
 
 class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping)
+
+        val database = ShoppingDatabase(this)
+        val repository = ShoppingRepository(database)
+        val factory = ShoppingViewModelFactory(repository)
+
+        val viewModel = ViewModelProviders.of(this, factory).get(ShoppingViewModel::class.java)
     }
 }
