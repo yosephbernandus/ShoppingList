5043633415b304db237b318f396cdff71d30fdd9
Yosephbernandus <yosephbernandus@gmail.com>
2022-03-19 13:10:41 +0700

    add room (Sqlite) model shopping item

--- app/src/main/java/mobile/android/shoppinglist/ShoppingActivity.kt
+++ app/src/main/java/mobile/android/shoppinglist/ShoppingActivity.kt
@@ -3,9 +3,9 @@ package mobile.android.shoppinglist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ShoppingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_shopping)
     }
 }
